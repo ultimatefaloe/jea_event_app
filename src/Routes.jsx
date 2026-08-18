e0144d640ecc9861f15e5edabb8dc11097846557
ultimatefaloe <ultimatefaloe@outlool.com>
2026-08-18 10:00:07 +0100

    event project init

---
+++ src/Routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Routes() {
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Start your event app</h1>
+    </div>
+  )
+}
+
+export default Routes
